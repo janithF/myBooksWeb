167274460afd3d6452dd9f9fc348b3b49e1fa0e2
Janith Fernando <janithnfernando@gmail.com>
2025-05-13 16:29:59 +0530

    Create two seperate components for grid card and list card

---
+++ src/components/BookListCard.tsx
@@ -0,0 +1,46 @@
+import type { Book } from "@/types";
+import { FaStar } from "react-icons/fa";
+import { FaPencil } from "react-icons/fa6";
+import { FaTrash } from "react-icons/fa6";
+
+interface Props {
+  book: Book;
+  onEdit: () => void;
+  onDelete: () => void;
+}
+
+const BookListCard = ({ book, onEdit, onDelete }: Props) => {
+  return (
+    <div className="relative bg-white shadow-md rounded-md overflow-hidden mb-2 flex max-w-[800px] group">
+      <img src={book.image} alt={book.title} className="h-24 w-24 mr-4" />
+      <div className="p-2 space-y-2">
+        <h2 className="text-xl font-light text-gray-800 line-clamp-2 m-0">{book.title}</h2>
+        <p className="text-sm text-gray-500">
+          by <span className="capitalize">{book.author.toLowerCase()}</span>
+        </p>
+        <div className="flex items-center gap-1 text-yellow-500">
+          <FaStar />
+          <span className="text-sm font-medium">{book.rating.toFixed(1)}</span>
+        </div>
+      </div>
+
+      {/* Hover Action Buttons */}
+      <div className="absolute right-0 w-30 h-full flex opacity-0 group-hover:opacity-100 bg-[#22202985] transition-opacity">
+        <div
+          className="flex justify-center items-center w-1/2 text-white font-bold text-xl px-2 py-4 hover:bg-[#3c9d9b91]  cursor-pointer"
+          onClick={onEdit}
+        >
+          <FaPencil />
+        </div>
+        <div
+          className="flex justify-center items-center w-1/2 font-bold text-xl  text-white px-2 py-3 hover:bg-[#ff000077] rounded cursor-pointer"
+          onClick={onDelete}
+        >
+          <FaTrash />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BookListCard;
--- src/components/BookListItem.tsx
+++ src/components/BookListItem.tsx
@@ -1,12 +1,12 @@
 import type { Book } from "../types";
-import { FaStar } from "react-icons/fa";
-import { FaPencil } from "react-icons/fa6";
-import { FaTrash } from "react-icons/fa6";
+
 import { useAppSelector } from "../app/hooks";
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "./ui/dialog";
 import BookForm from "./BookForm";
 import { useState } from "react";
 import AppAlertDialog from "./shared/AppAlertDialog";
+import BookGridCard from "./BookGridCard";
+import BookListCard from "./BookListCard";
 
 interface Props {
   book: Book;
@@ -19,7 +19,6 @@ const BookListItem = ({ book }: Props) => {
 
   // EDIT
   const onEditBook = () => {
-    console.log("Edit book", book);
     setOpenBookFormModal(true);
   };
 
@@ -36,72 +35,11 @@ const BookListItem = ({ book }: Props) => {
     console.log("Delete book", book);
   };
 
-  // LIST MODE
-  if (viewMode === "list") {
-    return (
-      <div className="relative bg-white shadow-md rounded-md overflow-hidden mb-2 flex max-w-[800px] group">
-        <img src={book.image} alt={book.title} className="h-24 w-24 mr-4" />
-        <div className="p-2 space-y-2">
-          <h2 className="text-xl font-light text-gray-800 line-clamp-2 m-0">{book.title}</h2>
-          <p className="text-sm text-gray-500">
-            by <span className="capitalize">{book.author.toLowerCase()}</span>
-          </p>
-          <div className="flex items-center gap-1 text-yellow-500">
-            <FaStar />
-            <span className="text-sm font-medium">{book.rating.toFixed(1)}</span>
-          </div>
-        </div>
-
-        {/* Hover Action Buttons */}
-        <div className="absolute right-0 w-30 h-full flex opacity-0 group-hover:opacity-100 bg-[#22202985] transition-opacity">
-          <div
-            className="flex justify-center items-center w-1/2 text-white font-bold text-xl px-2 py-4 hover:bg-[#3c9d9b91]  cursor-pointer"
-            onClick={onEditBook}
-          >
-            <FaPencil />
-          </div>
-          <div
-            className="flex justify-center items-center w-1/2 font-bold text-xl  text-white px-2 py-3 hover:bg-[#ff000077] rounded cursor-pointer"
-            onClick={onDeleteBook}
-          >
-            <FaTrash />
-          </div>
-        </div>
-      </div>
-    );
-  }
-
   // GRID MODE
   return (
-    <div className="relative bg-white shadow-md rounded-xl overflow-hidden group">
-      <img src={book.image} alt={book.title} className="h-60 w-full object-cover" />
-      <div className="p-4 space-y-2">
-        <h2 className="text-xl font-light text-gray-800 line-clamp-2 m-0">{book.title}</h2>
-        <p className="text-sm text-gray-500">
-          by <span className="capitalize">{book.author.toLowerCase()}</span>
-        </p>
-        <div className="flex items-center gap-1 text-yellow-500">
-          <FaStar />
-          <span className="text-sm font-medium">{book.rating.toFixed(1)}</span>
-        </div>
-      </div>
-
-      {/* Hover Action Buttons */}
-      <div className="absolute top-0 w-full flex justify-center opacity-0 group-hover:opacity-100 bg-[#22202985] transition-opacity">
-        <div
-          className="flex justify-center items-center h-15 w-1/2 text-white font-bold text-xl px-2 py-4 hover:bg-[#3c9d9b91]  cursor-pointer"
-          onClick={onEditBook}
-        >
-          <FaPencil />
-        </div>
-        <div
-          className="flex justify-center items-center h-15 w-1/2 font-bold text-xl  text-white px-2 py-3 hover:bg-[#ff000077] rounded cursor-pointer"
-          onClick={onDeleteBook}
-        >
-          <FaTrash />
-        </div>
-      </div>
-
+    <>
+      {viewMode === 'grid' ? <BookGridCard book={book} onEdit={onEditBook} onDelete={onDeleteBook} /> : <BookListCard book={book} onEdit={onEditBook} onDelete={onDeleteBook} />}
+      
       {/* Edit Form Modal */}
       <Dialog modal={true} open={openBookFormModal} onOpenChange={setOpenBookFormModal}>
         <DialogContent className="w-[450px] p-0 bg-app-background gap-0" onInteractOutside={(e) => e.preventDefault()}>
@@ -125,7 +63,8 @@ const BookListItem = ({ book }: Props) => {
         onCancel={closeOpenedDeleteAlert}
         onContinue={deleteBook}
       />
-    </div>
+    </>
+      
   );
 };
 
